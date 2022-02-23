14ecc99db80234b1e01c9d8a4d7c85a7c8bd0972
“copyPasteNinja” <abdugofir00@gmail.com>
2022-02-23 16:20:05 -0600

    IAM role is updated for ECSServiceRole

--- terraform/stacks/iam.tf
+++ terraform/stacks/iam.tf
@@ -20,6 +20,28 @@ resource "aws_iam_role_policy_attachment" "ecs_agent_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+resource "aws_iam_policy" "ecs_cross_account_assume_role" {
+  name = "ecs_crocc_account_assume_role"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : "sts:*",
+          "Resource" : "arn:aws:iam::100209986082:role/ECRPullImageCrossAccountRole"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_cross_account_assume_role_attachment" {
+  role       = aws_iam_role.ecs_agent.name
+  policy_arn = aws_iam_policy.ecs_cross_account_assume_role.arn
+}
+
 resource "aws_iam_instance_profile" "ecs_agent" {
   name = "ecs-agent-profile"
   role = aws_iam_role.ecs_agent.name
