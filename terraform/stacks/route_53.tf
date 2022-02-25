274b5bbb42d5e63b1a175195314277ea0209f88b
“copyPasteNinja” <abdugofir00@gmail.com>
2022-02-25 13:39:02 -0600

    temp: remov route_53 stuff temply

--- terraform/stacks/route_53.tf
+++ terraform/stacks/route_53.tf
@@ -1,37 +1,37 @@
-resource "aws_acm_certificate" "domain_certificate" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-}
+# resource "aws_acm_certificate" "domain_certificate" {
+#   domain_name       = var.domain_name
+#   validation_method = "DNS"
+# }
 
-resource "aws_route53_record" "domain_certificate_validation_record" {
-  for_each = {
-    for options in aws_acm_certificate.domain_certificate.domain_validation_options : options.domain_name => {
-      name   = options.resource_record_name
-      record = options.resource_record_value
-      type   = options.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "domain_certificate_validation_record" {
+#   for_each = {
+#     for options in aws_acm_certificate.domain_certificate.domain_validation_options : options.domain_name => {
+#       name   = options.resource_record_name
+#       record = options.resource_record_value
+#       type   = options.resource_record_type
+#     }
+#   }
 
-  name    = each.value.name
-  records = [each.value.record]
-  ttl     = 60
-  type    = each.value.type
-  zone_id = var.hosted_zone_id
-}
+#   name    = each.value.name
+#   records = [each.value.record]
+#   ttl     = 60
+#   type    = each.value.type
+#   zone_id = var.hosted_zone_id
+# }
 
-resource "aws_acm_certificate_validation" "domain_certificate_validation" {
-  certificate_arn         = aws_acm_certificate.domain_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.domain_certificate_validation_record : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "domain_certificate_validation" {
+#   certificate_arn         = aws_acm_certificate.domain_certificate.arn
+#   validation_record_fqdns = [for record in aws_route53_record.domain_certificate_validation_record : record.fqdn]
+# }
 
-resource "aws_route53_record" "domain_name" {
-  zone_id = var.hosted_zone_id
-  name    = var.domain_name
-  type    = "A"
+# resource "aws_route53_record" "domain_name" {
+#   zone_id = var.hosted_zone_id
+#   name    = var.domain_name
+#   type    = "A"
 
-  alias {
-    name                   = aws_alb.application_load_balancer.dns_name
-    zone_id                = aws_alb.application_load_balancer.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_alb.application_load_balancer.dns_name
+#     zone_id                = aws_alb.application_load_balancer.zone_id
+#     evaluate_target_health = true
+#   }
+# }
